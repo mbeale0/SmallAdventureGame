2f8fd59deaf05cf406ee67d10b16fd5e9708d711
Mason Beale <mbeale@cedarville.edu>
2022-10-04 23:24:05 -0400

    added grabable items

--- Assets/Scripts/ItemInteracter.cs
+++ Assets/Scripts/ItemInteracter.cs
@@ -7,6 +7,9 @@ public class ItemInteracter : MonoBehaviour
 {
     [SerializeField] private GameObject InteractCanvas = null;
     [SerializeField] private TextMeshProUGUI InteractText = null;
+    [SerializeField] private Transform playerHand = null;
+    [SerializeField] private bool isGrabable = false;
+    [SerializeField] private bool isPlaceable = false;
     // allow option to select if grabbing or placing
     // serialize field for hand and place objects(transforms probably
 
@@ -20,16 +23,27 @@ public class ItemInteracter : MonoBehaviour
     {
         if(canInteract && Input.GetKeyDown(KeyCode.E))
         {
-            //if grabbing
-                // Disableitem in scene
-                // show item in hand:
-                // for each child in characters hand
-                    // if hadn item tag == this tag
-                        // enable
-            // else placing
+            if (isGrabable)
+            {
+                foreach (Transform child in playerHand)
+                {
+                    if (child.CompareTag(tag))
+                    {
+                        child.gameObject.SetActive(true);
+                    }
+                }
+                InteractCanvas.SetActive(false);
+                Destroy(gameObject);
+            }
+            else if (isPlaceable)
+            {
+                // else placing
                 // diable item in hand
                 // activate item in scene
                 // activate in scene action(door opening/winning)
+            }
+
+
         }
     }
     private void OnMouseEnter()
